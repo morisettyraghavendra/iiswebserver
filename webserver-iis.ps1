23bede5ce5346ad81b676366fb41936d4d49a31d
morisettyraghavendra <98275392+morisettyraghavendra@users.noreply.github.com>
2022-01-30 23:00:46 +0530

    Add files via upload

---
+++ webserver-iis.ps1
@@ -0,0 +1,5 @@
+﻿#power shell script to install we server on vertual machine using run command in azure portal(in virtual machine options we find "run command" option )
+
+Import-Module Servermanager
+Install-WindowsFeature -Name "Web-Server" -IncludeManagementTools
+Set-Content -Path "C:\inetpub\wwwroot\Default.html" -Value "This is the server: $($env:COMPUTERNAME)!"
